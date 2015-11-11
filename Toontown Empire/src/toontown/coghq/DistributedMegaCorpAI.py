69100a4cec8cb9eed137134eae923a56fe308361
Spiderlover <darthvader438@gmail.com>
2015-11-11 16:47:29 -0500

    Deactivate SOS Reward
    Deactivate SOS Reward for now

--- Toontown Empire/src/toontown/coghq/DistributedMegaCorpAI.py
+++ Toontown Empire/src/toontown/coghq/DistributedMegaCorpAI.py
@@ -1,7 +1,7 @@
 from direct.directnotify import DirectNotifyGlobal
 import DistributedFactoryAI
 from toontown.toon import NPCToons
-import random
+#import random
 
 class DistributedMegaCorpAI(DistributedFactoryAI.DistributedFactoryAI):
     notify = DirectNotifyGlobal.directNotify.newCategory('DistributedBrutalFactoryAI')
@@ -9,15 +9,15 @@ class DistributedMegaCorpAI(DistributedFactoryAI.DistributedFactoryAI):
     def __init__(self, air, factoryId, zoneId, entranceId, avIds):
         DistributedFactoryAI.DistributedFactoryAI.__init__(self, air, factoryId, zoneId, entranceId, avIds)
 
-    def setVictors(self, victorIds):
-        DistributedFactoryAI.DistributedFactoryAI.setVictors(self, victorIds)
+    #def setVictors(self, victorIds):
+        #DistributedFactoryAI.DistributedFactoryAI.setVictors(self, victorIds)
 
-        activeVictors = []
-        for victorId in victorIds:
-            toon = self.air.doId2do.get(victorId)
-            if toon is not None:
-                activeVictors.append(toon)
-        npcId = random.choice(NPCToons.npcFriendsMinMaxStars(3, 3))
-        for toon in activeVictors:
-            toon.attemptAddNPCFriend(npcId)
-            toon.d_setSystemMessage(0, 'You got a %s SOS card.' % (NPCToons.getNPCName(npcId)))
+        #activeVictors = []
+        #for victorId in victorIds:
+            #toon = self.air.doId2do.get(victorId)
+            #if toon is not None:
+                #activeVictors.append(toon)
+        #npcId = random.choice(NPCToons.npcFriendsMinMaxStars(3, 3))
+        #for toon in activeVictors:
+            #toon.attemptAddNPCFriend(npcId)
+            #toon.d_setSystemMessage(0, 'You got a %s SOS card.' % (NPCToons.getNPCName(npcId)))
