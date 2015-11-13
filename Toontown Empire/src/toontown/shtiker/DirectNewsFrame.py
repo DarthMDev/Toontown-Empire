e5c2623d626e58e34ad30f3f138063cc6df27349
Michael <mgracer48@yahoo.com>
2015-11-13 23:36:02 +0100

    fix news

--- Toontown Empire/src/toontown/shtiker/DirectNewsFrame.py
+++ Toontown Empire/src/toontown/shtiker/DirectNewsFrame.py
@@ -169,8 +169,8 @@ class DirectNewsFrame(DirectObject.DirectObject):
         self.mainFrame = DirectFrame(parent=self.backFrame, frameSize=self.FrameDimensions, frameColor=(1, 0, 0, 1))
 
     def activate(self):
-        if hasattr(self, 'createdTime') and self.createdTime < base.cr.inGameNewsMgr.getLatestIssue() and self.NewsOverHttp and not self.redownloadingNews:
-            self.redownloadNews()
+        if hasattr(self, 'createdTime') and self.createdTime and self.NewsOverHttp and not self.redownloadingNews:
+            self.active = False
         else:
             self.addDownloadingTextTask()
         if self.needsParseNews and not self.redownloadingNews:
@@ -369,7 +369,7 @@ class DirectNewsFrame(DirectObject.DirectObject):
                 self.redownloadNews()
 
     def getInGameNewsUrl(self):
-        result = base.config.GetString('fallback-news-url', 'http://cdn.toontown.disney.go.com/toontown/en/gamenews/')
+        result = base.config.GetString('fallback-news-url', 'http://cdn.toontown.disney.go.com/toontown/en/gamenews/') # TODO: Place our news URL here.
         override = base.config.GetString('in-game-news-url', '')
         if override:
             self.notify.info('got an override url,  using %s for in game news' % override)
