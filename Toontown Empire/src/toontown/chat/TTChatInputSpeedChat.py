f2ece9e313eb8867eb66debb3c47adb2baec5e10
mgracer48 <mgracer48@users.noreply.github.com>
2015-11-14 10:13:19 +0100

    Update TTChatInputSpeedChat.py

--- Toontown Empire/src/toontown/chat/TTChatInputSpeedChat.py
+++ Toontown Empire/src/toontown/chat/TTChatInputSpeedChat.py
@@ -1,5 +1,5 @@
 from direct.gui.DirectGui import *
-from pandac.PandaModules import *
+from panda3d.core import *
 from src.otp.speedchat.SpeedChatTypes import *
 from src.toontown.speedchat.TTSpeedChatTypes import *
 from src.otp.speedchat.SpeedChat import SpeedChat
