fc7f3f65adca387595ae097f2006db832551051b
mgracer48 <mgracer48@users.noreply.github.com>
2015-11-14 09:41:30 +0100

    Update QuestPoster.py

--- Toontown Empire/src/toontown/quest/QuestPoster.py
+++ Toontown Empire/src/toontown/quest/QuestPoster.py
@@ -1,5 +1,5 @@
 from direct.gui.DirectGui import *
-from pandac.PandaModules import *
+from panda3d.core import *
 import Quests
 from src.toontown.toon import NPCToons
 from src.toontown.toon import ToonHead
