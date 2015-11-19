f027758dbf21b813b1d6df23355df70d75568db0
Michael <mgracer48@yahoo.com>
2015-11-20 00:02:15 +0100

    add this back

--- Toontown Empire/src/toontown/hood/TTHoodAI.py
+++ Toontown Empire/src/toontown/hood/TTHoodAI.py
@@ -3,6 +3,8 @@ from toontown.safezone import ButterflyGlobals
 from toontown.safezone import DistributedButterflyAI
 from toontown.safezone import DistributedTrolleyAI
 from toontown.toonbase import ToontownGlobals
+from toontown.toon import NPCToons
+from toontown.toonbase import TTLocalizer
 from toontown.ai import DistributedEffectMgrAI
 
 class TTHoodAI(HoodAI.HoodAI):
@@ -23,6 +25,10 @@ class TTHoodAI(HoodAI.HoodAI):
         if simbase.config.GetBool('want-butterflies', True):
             self.createButterflies()
 
+        NPCToons.createNPC(
+            simbase.air, 2021,
+            (ToontownGlobals.ToontownCentral, TTLocalizer.NPCToonNames[2021], ('rls', 'ls', 'l', 'm', 26, 0, 26, 26, 152, 27, 139, 27, 58, 27, 0), 'm', 1, NPCToons.NPC_GLOVE),
+             ToontownGlobals.ToontownCentral, posIndex=0)
         self.trickOrTreatMgr = DistributedEffectMgrAI.DistributedEffectMgrAI(self.air, ToontownGlobals.HALLOWEEN, 12)
         self.trickOrTreatMgr.generateWithRequired(2649) # All Fun and Games Shop, Silly Street
 
