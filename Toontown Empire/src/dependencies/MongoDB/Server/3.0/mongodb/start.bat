853cd9201f36532900c7f1ff6fb763f45cb73ff8
Spiderlover <darthvader438@gmail.com>
2015-11-20 21:05:11 -0500

    Fix MongoDB

--- Toontown Empire/src/dependencies/MongoDB/Server/3.0/mongodb/start.bat
+++ Toontown Empire/src/dependencies/MongoDB/Server/3.0/mongodb/start.bat
@@ -1,5 +1,5 @@
 @echo off
-title Toontown Empire Development MongoDB
+title Toontown Crystal Development MongoDB
 
 "C:\Program Files (x86)\MongoDB\Server\3.0\bin\mongod.exe" --dbpath .\
 
--- Toontown Empire/src/dev/win32/!START FIRST Estates Server & Gardening.bat
+++ Toontown Empire/src/dev/win32/!START FIRST Estates Server & Gardening.bat
@@ -1,7 +1,7 @@
 @echo off
 title TTE MongoDB
 
-cd ../../
+cd ../../../
 
 :main
 "src/dependencies/MongoDB\Server\3.0\bin\mongod.exe" --dbpath src/dependencies/MongoDB/GardeningDatabase
