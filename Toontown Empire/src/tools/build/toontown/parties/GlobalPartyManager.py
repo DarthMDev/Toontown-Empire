d13ca9002a35ff1e4a321bd5f17274b6a329f3da
Michael <mgracer48@yahoo.com>
2015-11-22 12:18:28 +0100

    delete build files

--- Toontown Empire/src/tools/build/toontown/parties/GlobalPartyManager.py
+++
@@ -1,5 +0,0 @@
-from direct.distributed.DistributedObjectGlobal import DistributedObjectGlobal
-
-
-class GlobalPartyManager(DistributedObjectGlobal):
-    notify = directNotify.newCategory('GlobalPartyManager')
