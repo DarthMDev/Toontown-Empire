f45e8e1085d46e977f115ae243ec3d61856ce2df
Michael <mgracer48@yahoo.com>
2015-12-12 15:03:30 +0100

    fix game

--- Toontown-Empire/src/toontown/quest/Quests.py
+++ Toontown-Empire/src/toontown/quest/Quests.py
@@ -1800,7 +1800,7 @@ class CogQuest(LocationBasedQuest):
     def getNumCogs(self):
         return self.quest[1]
 
-    def getCompletionStatus(self, questDesc, npc = None):
+    def getCompletionStatus(self, *args questDesc, npc = None):
         questId, fromNpcId, toNpcId, rewardId, toonProgress = questDesc
         questComplete = toonProgress >= self.getNumCogs()
         return getCompleteStatusWithNpc(questComplete, toNpcId, npc)
