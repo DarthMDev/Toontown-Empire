8249c9c5fb1ec0d93fcf4a50cfeda30bce8f04cd
Spiderlover <darthvader438@gmail.com>
2016-01-19 19:51:39 -0500

    Switch to Panda3D.core

--- src/otp/ai/AIBaseGlobal.py
+++ src/otp/ai/AIBaseGlobal.py
@@ -24,7 +24,7 @@ taskMgr.finalInit()
 
 # The VirtualFileSystem, which has already initialized, doesn't see the mount
 # directives in the config(s) yet. We have to force it to load those manually:
-from pandac.PandaModules import VirtualFileSystem, ConfigVariableList, Filename
+from panda3d.core import VirtualFileSystem, ConfigVariableList, Filename
 vfs = VirtualFileSystem.getGlobalPtr()
 mounts = ConfigVariableList('vfs-mount')
 for mount in mounts:
