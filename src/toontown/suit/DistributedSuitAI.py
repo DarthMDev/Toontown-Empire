2764edf1bc018e5f7be8fba67fa4373cb70008ee
Dynamite106 <joshmercado1@aol.com>
2016-01-22 01:59:49 -0500

    Fixed mac + linux support!

--- src/toontown/suit/DistributedSuitAI.py
+++ src/toontown/suit/DistributedSuitAI.py
@@ -8,7 +8,7 @@ import SuitPlannerBase
 import SuitBase
 import SuitDialog
 import SuitDNA
-from toontown.suit.SuitLegList import *
+from SuitLegList import *
 from direct.directnotify import DirectNotifyGlobal
 from toontown.battle import SuitBattleGlobals
 from toontown.building import FADoorCodes
@@ -216,7 +216,7 @@ class DistributedSuitAI(DistributedSuitBaseAI.DistributedSuitBaseAI):
         self.setPathPosition(0, self.pathStartTime)
         self.pathState = 1
         self.currentLeg = 0
-        self.zoneId = self.legList.getZoneId(0)
+#        self.zoneId = ZoneUtil.getTrueZoneId(self.legList.getZoneId(0), self.branchId)
         self.legType = self.legList.getType(0)
         if self.notify.getDebug():
             self.notify.debug('creating suit in zone %s' % self.zoneId)
@@ -230,12 +230,10 @@ class DistributedSuitAI(DistributedSuitBaseAI.DistributedSuitBaseAI):
         nextLeg = self.legList.getLegIndexAtTime(elapsed, self.currentLeg)
         numLegs = self.legList.getNumLegs()
         if self.currentLeg != nextLeg:
-            if nextLeg >= numLegs:
-                self.flyAwayNow()
-                return Task.done
             self.currentLeg = nextLeg
             self.__beginLegType(self.legList.getType(nextLeg))
             zoneId = self.legList.getZoneId(nextLeg)
+#            zoneId = ZoneUtil.getTrueZoneId(zoneId, self.branchId)
             if zoneId:
                 self.__enterZone(zoneId)
             self.notify.debug('Suit %s reached leg %s of %s in zone %s.' % (self.getDoId(),
@@ -362,9 +360,8 @@ class DistributedSuitAI(DistributedSuitBaseAI.DistributedSuitBaseAI):
         if not self.sp.buildingMgr.isSuitBlock(blockNumber):
             self.notify.debug('Suit %s taking over building %s in %s' % (self.getDoId(), blockNumber, self.zoneId))
             difficulty = self.getActualLevel() - 1
-
-            dept = SuitDNA.getSuitDept(self.dna.name)
             if self.buildingDestinationIsCogdo:
-                self.sp.cogdoTakeOver(blockNumber, difficulty, self.buildingHeight, dept)
+                self.sp.cogdoTakeOver(blockNumber, difficulty, self.buildingHeight)
             else:
+                dept = SuitDNA.getSuitDept(self.dna.name)
                 self.sp.suitTakeOver(blockNumber, dept, difficulty, self.buildingHeight)
