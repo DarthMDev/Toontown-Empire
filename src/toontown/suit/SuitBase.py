9c507ff0153082d90b0755986196155b131551d4
Michael <mgracer48@yahoo.com>
2016-01-24 23:28:34 +0100

    fix crash

--- src/toontown/suit/SuitBase.py
+++ src/toontown/suit/SuitBase.py
@@ -1,5 +1,5 @@
 import SuitDNA
-from SuitLegList import *
+from toontown.dna.SuitLegList import *
 import SuitTimings
 from direct.directnotify import DirectNotifyGlobal
 from direct.distributed.ClockDelta import *
