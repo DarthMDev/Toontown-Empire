75fb4ebbbf37382d32563eea8692c5c2a4d42753
Michael <mgracer48@users.noreply.github.com>
2016-01-30 09:53:17 +0100

    Update VersionUpdater.py

--- src/dev/tools/Version Updater/VersionUpdater.py
+++ src/dev/tools/Version Updater/VersionUpdater.py
@@ -2,3 +2,13 @@ import os
 os.chdir('../../../')
 
 from dependencies.astron.config import *
+#TODO make version equal to end of TTE- try using indexes
+if version is None:
+    version = 1.0
+ask = raw_input('Are you sure you want to change version number by .1? Type yes or no: '
+ask = str(ask)
+ask = ask.lower()
+if ask == 'yes':
+    version += 0.1
+elif ask == 'no':
+    exit()
