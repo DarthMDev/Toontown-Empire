7434ee1f48bb2b0852dbf061e32d5a54201f098e
malverde <malverdedeyum@hotmail.com>
2016-02-06 23:16:32 +0000

    toontown.dna files fixed

--- src/toontown/dna/DNAParser.py
+++ src/toontown/dna/DNAParser.py
@@ -7,8 +7,6 @@ from DNAGroup import DNAGroup
 from DNAVisGroup import DNAVisGroup
 from DNADoor import DNADoor
 
-from libpandadna import *
-
 class DNABulkLoader:
     def __init__(self, storage, files):
         self.dnaStorage = storage
@@ -22,22 +20,35 @@ class DNABulkLoader:
         del self.dnaFiles
 
 def loadDNABulk(dnaStorage, file):
-    dnaLoader = DNALoader()
-    file = '/' + file
+    dnaLoader = DNALoader.DNALoader()
+    if __debug__:
+        file = 'resources/' + file
+    else:
+        file = '/' + file
     dnaLoader.loadDNAFile(dnaStorage, file)
+    dnaLoader.destroy()
 
 def loadDNAFile(dnaStorage, file):
     print 'Reading DNA file...', file
-    dnaLoader = DNALoader()
-    file = '/' + file
+    dnaLoader = DNALoader.DNALoader()
+    if __debug__:
+        file = 'resources/' + file
+    else:
+        file = '/' + file
     node = dnaLoader.loadDNAFile(dnaStorage, file)
+    dnaLoader.destroy()
     if node.node().getNumChildren() > 0:
         return node.node()
+    return None
 
 def loadDNAFileAI(dnaStorage, file):
-    dnaLoader = DNALoader()
-    file = '/' + file
+    dnaLoader = DNALoader.DNALoader()
+    if __debug__:
+        file = 'resources/' + file
+    else:
+        file = '/' + file
     data = dnaLoader.loadDNAFileAI(dnaStorage, file)
+    dnaLoader.destroy()
     return data
 
 def setupDoor(doorNodePath, parentNode, doorOrigin, dnaStore, block, color):
