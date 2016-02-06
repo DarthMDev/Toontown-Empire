6e9fe109f8db61ff3679b4c55281b057c014afb2
malverde <malverdedeyum@hotmail.com>
2016-02-06 22:46:27 +0000

    Revert "Change to DNADoor"
    This reverts commit eeae4484cdf6c8d19716586fb8738e90a0199cfb.

--- src/toontown/building/DistributedHQInterior.py
+++ src/toontown/building/DistributedHQInterior.py
@@ -8,7 +8,7 @@ from panda3d.core import *
 from toontown.toonbase import TTLocalizer
 from toontown.toonbase.ToonBaseGlobal import *
 from toontown.toonbase.ToontownGlobals import *
-from toontown.dna.DNAParser import DNADoor
+from toontown.dna.DNAParser import *
 from toontown.toon.DistributedNPCToonBase import DistributedNPCToonBase
 
 
@@ -27,7 +27,6 @@ class DistributedHQInterior(DistributedObject.DistributedObject):
         self.interior = loader.loadModel('phase_3.5/models/modules/HQ_interior')
         self.interior.reparentTo(render)
         self.interior.find('**/cream').hide()
-        self.interior.find('**/crashed_piano').hide()
         self.buildLeaderBoard()
 
     def announceGenerate(self):
@@ -156,7 +155,7 @@ class DistributedHQInterior(DistributedObject.DistributedObject):
             doorOrigin.setPos(doorOrigin, 0, -0.025, 0)
             doorColor = self.randomGenerator.choice(self.colors['TI_door'])
             triggerId = str(self.block) + '_' + doorOriginIndexStr
-            DNADoor.setupDoor(doorNP, newNodePath, doorOrigin, self.dnaStore, triggerId, doorColor)
+            setupDoor(doorNP, newNodePath, doorOrigin, self.dnaStore, triggerId, doorColor)
             doorFrame = doorNP.find('door_*_flat')
             doorFrame.setColor(doorColor)
         del self.dnaStore
