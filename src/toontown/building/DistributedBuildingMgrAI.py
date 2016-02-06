e7ba6742f5dfc360e0ac0b0bdd377a4cb3649b34
malverde <malverdedeyum@hotmail.com>
2016-02-06 22:46:24 +0000

    Revert "Error Still  Proceeds On Doors all over toontown :("
    This reverts commit 145ba7f5e184a647200928798b19e1912145be30.

--- src/toontown/building/DistributedBuildingMgrAI.py
+++ src/toontown/building/DistributedBuildingMgrAI.py
@@ -145,7 +145,6 @@ class DistributedBuildingMgrAI:
     def newHQBuilding(self, blockNumber):
         dnaStore = self.air.dnaStoreMap[self.canonicalBranchId]
         exteriorZoneId = dnaStore.getZoneFromBlockNumber(blockNumber)
-        exteriorZoneId = ZoneUtil.getTrueZoneId(exteriorZoneId, self.branchId)
         interiorZoneId = (self.branchId - (self.branchId%100)) + 500 + blockNumber
         building = HQBuildingAI.HQBuildingAI(
             self.air, exteriorZoneId, interiorZoneId, blockNumber)
@@ -155,7 +154,6 @@ class DistributedBuildingMgrAI:
     def newGagshopBuilding(self, blockNumber):
         dnaStore = self.air.dnaStoreMap[self.canonicalBranchId]
         exteriorZoneId = dnaStore.getZoneFromBlockNumber(blockNumber)
-        exteriorZoneId = ZoneUtil.getTrueZoneId(exteriorZoneId, self.branchId)
         interiorZoneId = (self.branchId - (self.branchId%100)) + 500 + blockNumber
         building = GagshopBuildingAI.GagshopBuildingAI(
             self.air, exteriorZoneId, interiorZoneId, blockNumber)
@@ -165,7 +163,6 @@ class DistributedBuildingMgrAI:
     def newPetshopBuilding(self, blockNumber):
         dnaStore = self.air.dnaStoreMap[self.canonicalBranchId]
         exteriorZoneId = dnaStore.getZoneFromBlockNumber(blockNumber)
-        exteriorZoneId = ZoneUtil.getTrueZoneId(exteriorZoneId, self.branchId)
         interiorZoneId = (self.branchId - (self.branchId%100)) + 500 + blockNumber
         building = PetshopBuildingAI.PetshopBuildingAI(
             self.air, exteriorZoneId, interiorZoneId, blockNumber)
@@ -175,7 +172,6 @@ class DistributedBuildingMgrAI:
     def newKartShopBuilding(self, blockNumber):
         dnaStore = self.air.dnaStoreMap[self.canonicalBranchId]
         exteriorZoneId = dnaStore.getZoneFromBlockNumber(blockNumber)
-        exteriorZoneId = ZoneUtil.getTrueZoneId(exteriorZoneId, self.branchId)
         interiorZoneId = (self.branchId - (self.branchId%100)) + 500 + blockNumber
         building = KartShopBuildingAI.KartShopBuildingAI(
             self.air, exteriorZoneId, interiorZoneId, blockNumber)
