919507efb8342478091c7df454e51b0d630b37a5
Dynamite106 <joshmercado1@aol.com>
2016-02-13 15:41:32 -0500

    New dev server up and running!

--- src/dependencies/astron/darwin/start_uberdog_server.sh
+++
@@ -1,3 +0,0 @@
-cd ../../../
-export PYTHONPATH=dependencies/mac/lib:$PYTHONPATH
-ppython -m toontown.uberdog.ServiceStart --base-channel 1000000 --max-channels 999999 --stateserver 4002 --astron-ip 127.0.0.1:7100 --eventlogger-ip 127.0.0.1:7198
