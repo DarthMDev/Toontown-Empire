f1cbf105aaa90bcff45459997f96a69f824401f9
malverde <malverdedeyum@hotmail.com>
2016-02-14 19:22:21 +0000

    For some reason the otp folder is been buggy for uberdog on other peoples comps

--- src/otp/uberdog/GlobalOtpObject.py
+++
@@ -1,5 +0,0 @@
-from direct.distributed.DistributedObjectGlobal import DistributedObjectGlobal
-
-
-class GlobalOtpObject(DistributedObjectGlobal):
-    notify = directNotify.newCategory('GlobalOtpObject')
