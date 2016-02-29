681085cb28e178239041c5550ce8ba6e2d768a46
malverde <malverdedeyum@hotmail.com>
2016-02-29 22:16:10 +0000

    Revert "Garbage Collection"
    This reverts commit 055ba1894b26be3bd5cd806d3bae9deb63ad257e.

--- src/toontown/ai/ServiceStart.py
+++ src/toontown/ai/ServiceStart.py
@@ -51,10 +51,6 @@ loadPrcFileData('Command-line', localconfig)
 
 from otp.ai.AIBaseGlobal import *
 
-# We need to disable garbage collection during the AI startup process
-import gc
-gc.disable()
-
 from toontown.ai.ToontownAIRepository import ToontownAIRepository
 simbase.air = ToontownAIRepository(config.GetInt('air-base-channel', 401000000),
                                    config.GetInt('air-stateserver', 4002),
