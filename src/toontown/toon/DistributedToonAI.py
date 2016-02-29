096943ea1432c59b26ed9cce462b6296b1794130
malverde <malverdedeyum@hotmail.com>
2016-02-29 22:18:05 +0000

    Revert "Fixes error when making friends"
    This reverts commit 10aaeb5aa46f6fe65060490ade6f01bb938b2f66.

--- src/toontown/toon/DistributedToonAI.py
+++ src/toontown/toon/DistributedToonAI.py
@@ -671,6 +671,8 @@ class DistributedToonAI(DistributedPlayerAI.DistributedPlayerAI, DistributedSmoo
         
         return False
     
+    def getAchievements(self):
+        return self.achievements
 
     def isTrunkFull(self, extraAccessories = 0):
         numAccessories = (len(self.hatList) + len(self.glassesList) + len(self.backpackList) + len(self.shoesList)) / 3
