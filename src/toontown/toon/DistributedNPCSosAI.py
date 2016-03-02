6e7e8ed26cab0cb0b8b037d2bd10544d66050cee
ponyboy837 <ponyboy837@gmail.com>
2016-03-02 15:32:31 -0500

    Sos Shop Keeper MEGA UPDATE
    Added Text. Added Sos Options and so MUCH MORE!

--- src/toontown/toon/DistributedNPCSosAI.py
+++ src/toontown/toon/DistributedNPCSosAI.py
@@ -3,17 +3,36 @@ import SosShopGlobals, DistributedNPCToonBaseAI, random, math, NPCToons
 
 class DistributedNPCSosAI(DistributedNPCToonBaseAI.DistributedNPCToonBaseAI):
 
-    def roll(self, amount):
+    def roll(self, amount, cost, type):
         av = simbase.air.doId2do.get(self.air.getAvatarIdFromSender())
 
         if not av:
             return
             
         #This does not include FO sos cards.
-        NPCIdList = [2001, 2132, 2121, 2011, 3007, 1001, 3112, 1323, 2308, 4119, 4219, 4115, 1116, 2311, 4140, 3137, 4327, 4230, 3135, 2208, 5124, 2003, 2126, 4007, 1315, 5207, 3129, 4125, 1329]
+        FullNPCIdList = [2001, 2132, 2121, 2011, 3007, 1001, 3112, 1323, 2308, 4119, 4219, 4115, 1116, 2311, 4140, 3137, 4327, 4230, 3135, 2208, 5124, 2003, 2126, 4007, 1315, 5207, 3129, 4125, 1329]
+        #Only 3 Star Sos Cards
+        ThreeStarNPCIdList = [2121, 1001, 1323, 2308, 4115, 4140, 2126, 4007, 1315, 5207, 3129, 4125, 1329]
+        #Only 4 Star Sos Cards
+        FourStarNPCIdList = [2132, 3007, 4219, 2311, 3137, 4327, 4230, 3135, 2208, 5124]
+        #Only 5 Star Sos Cards
+        FiveStarNPCIdList = [2001, 2011, 4119, 1116, 3112, 2003]
         
         count = amount
-        npcIdList = list(NPCIdList)
+        cost = cost
+        type = type
+        
+        if type == 2:
+         npcIdList = list(FullNPCIdList)
+        elif type == 3:
+         npcIdList = list(ThreeStarNPCIdList)
+        elif type == 4:
+         npcIdList = list(FourStarNPCIdList)
+        elif type == 5:
+         npcIdList = list(FiveStarNPCIdList)
+        else:
+         npcIdList = list(FullNPCIdList)
+        
         npcId = random.choice(npcIdList)
                   
         if npcId not in NPCToons.npcFriends:
@@ -25,7 +44,6 @@ class DistributedNPCSosAI(DistributedNPCToonBaseAI.DistributedNPCToonBaseAI):
             self.sendUpdate('rollResult', [SosShopGlobals.FULL_SOS])
             return
 
-        cost = SosShopGlobals.CostPerSOS
 
         if cost > av.getTotalMoney():
             self.sendUpdate('rollResult', [SosShopGlobals.NOT_ENOUGH_MONEY])
